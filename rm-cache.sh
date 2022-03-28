426b6a2b7ded9dd897df7910cea6ec49b1f981f1
van.duong@redweb.dk <van.duong@redweb.dk>
2022-03-28 10:09:04 +0700

    init

--- rm-cache.sh
+++ rm-cache.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
 
-read_var() {
-    VAR=$(grep $1 $2 | xargs)
-    IFS="=" read -ra VAR <<< "$VAR"
-    echo ${VAR[1]}
-}
-
-APP_URL=$(read_var APP_URL .env)/
-WEB_DIR=$(pwd)/public/
-
-RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
-echo "<?php opcache_reset(); echo 'Cleared opcache nginx!'.PHP_EOL; ?>" > ${WEB_DIR}${RANDOM_NAME}.php
 php artisan cache:clear
 php artisan view:clear
 php artisan config:clear
 php artisan route:cache
 php artisan queue:restart
-curl ${APP_URL}${RANDOM_NAME}.php
-rm ${WEB_DIR}${RANDOM_NAME}.php
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Media')->group(function(){
     Route::post('media', 'MediaController@media')->name('media.store');
 });
-
-Route::get('/', function () {
-    return view('welcome');
-});
